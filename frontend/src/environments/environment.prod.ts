0fe6f28e362e67c4a62f5e87de7cb78b89b99118
Thomas <lhomme.thomas@pm.me>
2021-07-08 11:36:34 +0000

    docker file nginx

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   serverport: 4200,
   production: true,
   adminmail: ['lhomme.thomas@pm.me', 'Pierre@bouvier-muller.fr', 'pierre@bouvier-muller.fr'],
-  serverUrl: 'https://100lieuxnourriciers.fr:3000/'
+  serverUrl: 'http://100lieuxnourriciers.fr:3000/'
 };
