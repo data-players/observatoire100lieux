2f2eeb336cbbe29768a8bdec5e9fb926cfa9d78a
Thomas Lhomme <blabla@blabla.com>
2021-07-08 16:58:55 +0200

    env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   serverport: 4200,
   production: true,
   adminmail: ['lhomme.thomas@pm.me', 'Pierre@bouvier-muller.fr', 'pierre@bouvier-muller.fr'],
-  serverUrl: 'https://100lieuxnourriciers.fr:3000/'
+  serverUrl: 'https://data.100lieuxnourriciers.fr/'
 };
