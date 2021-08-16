fd2cd49d8d41af085cc4e68b599414f79a084984
Thomas Lhomme <lhomme.thomas@gmail.com>
2021-08-16 15:44:37 +0200

    gitIgnore

--- middleware/config.js
+++ middleware/config.js
@@ -1,25 +1,22 @@
 require('dotenv-flow').config();
 
 module.exports = {
-SEMAPPS_HOME_URL: 'http://localhost:3000/',
-SEMAPPS_SPARQL_ENDPOINT: 'http://localhost:3030/',
-//SEMAPPS_SPARQL_ENDPOINT: 'http://fuseki:3030/',
-SEMAPPS_MAIN_DATASET: 'localData',
-SEMAPPS_JENA_USER: 'admin',
-SEMAPPS_JENA_PASSWORD: 'admin',
-
-SEMAPPS_OIDC_ISSUER: 'https://login.lescommuns.org/auth/realms/master/',
-SEMAPPS_OIDC_CLIENT_ID: 'semapps',
-SEMAPPS_OIDC_CLIENT_SECRET: '8b90b5f1-bb15-4438-9f04-d61262705430',
-SEMAPPS_QUEUE_SERVICE_URL: 'redis://redis:6379/0',
-SEMAPPS_REDIS_CACHE_URL:'redis://redis:6379/0',
-SEMAPPS_BACKUP_FUSEKI_DATASETS_PATH: '/Users/thomaslhomme/Documents/Dev/cheminstransition/observatoire100lieux/data/fuseki/backups/cronapp',
-
-SEMAPPS_FROM_EMAIL:"noreply@100lieuxnourriciers.fr",
-SEMAPPS_FROM_NAME: "100LieuxNourriciers",
-SEMAPPS_SMTP_HOST: "ssl0.ovh.net",
-SEMAPPS_SMTP_PORT: 465,
-SEMAPPS_SMTP_SECURE:true,
-SEMAPPS_SMTP_USER:"noreply@100lieuxnourriciers.fr",
-SEMAPPS_SMTP_PASS:"cent100!!",
-}
+    SEMAPPS_HOME_URL:  process.env.SEMAPPS_HOME_URL,
+    SEMAPPS_SPARQL_ENDPOINT: process.env.SEMAPPS_SPARQL_ENDPOINT,
+    SEMAPPS_MAIN_DATASET: process.env.SEMAPPS_MAIN_DATASET,
+    SEMAPPS_JENA_USER:  process.env.SEMAPPS_JENA_USER,
+    SEMAPPS_JENA_PASSWORD:  process.env.SEMAPPS_JENA_PASSWORD,
+    SEMAPPS_OIDC_ISSUER: process.env.SEMAPPS_OIDC_ISSUER,
+    SEMAPPS_OIDC_CLIENT_ID: process.env.SEMAPPS_OIDC_CLIENT_ID,
+    SEMAPPS_OIDC_CLIENT_SECRET: process.env.SEMAPPS_OIDC_CLIENT_SECRET,
+    SEMAPPS_QUEUE_SERVICE_URL:  process.env.SEMAPPS_QUEUE_SERVICE_URL,
+    SEMAPPS_REDIS_CACHE_URL: process.env.SEMAPPS_REDIS_CACHE_URL,
+    SEMAPPS_BACKUP_FUSEKI_DATASETS_PATH: process.env.SEMAPPS_BACKUP_FUSEKI_DATASETS_PATH,
+    SMTP_HOST: process.env.SEMAPPS_SMTP_HOST,
+    SMTP_PORT: parseInt(process.env.SEMAPPS_SMTP_PORT, 10),
+    SMTP_SECURE: process.env.SEMAPPS_SMTP_SECURE === 'true',
+    SMTP_USER: process.env.SEMAPPS_SMTP_USER,
+    SMTP_PASS: process.env.SEMAPPS_SMTP_PASS,
+    FROM_EMAIL: process.env.SEMAPPS_FROM_EMAIL,
+    FROM_NAME: process.env.SEMAPPS_FROM_NAME
+};
