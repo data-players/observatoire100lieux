58c2686518b506005c3d53b4a61bd53d226a62fc
Thomas Lhomme <lhomme.thomas@gmail.com>
2021-10-18 15:02:05 +0200

    Fix variable prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   adminmail: ['lhomme.thomas@pm.me', 'romain.guitet@afaup.org' ,'Pierre@bouvier-muller.fr', 'pierre@bouvier-muller.fr'],
   serverUrl: 'https://data.100lieuxnourriciers.fr/',
-  frontUrl: 'https://100lieuxnourriciers.fr/'
-
+  frontUrl: 'https://100lieuxnourriciers.fr/',
+  adresseUrl: 'https://api-adresse.data.gouv.fr/search/'
 };
