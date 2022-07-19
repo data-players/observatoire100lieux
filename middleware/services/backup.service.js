f36628190be50608b0a1ad0eb24c7b519b06a47a
BastienSig <bastien.siguier@gmail.com>
2022-07-19 08:29:20 +0200

    add ftp service

--- middleware/services/backup.service.js
+++ middleware/services/backup.service.js
@@ -1,21 +1,62 @@
-const BackupService = require('@semapps/backup');
 const path = require('path');
-const CONFIG = require('../config');
+const BackupService = require('@semapps/backup');
+const fs = require('fs');
+let Client = require('ssh2-sftp-client');
+require('dotenv-flow').config();
+const CONFIG = require('../config')
+
+console.log('fuseki-backup path',path.resolve(__dirname, '../fuseki-backups'));
 
 module.exports = {
-    name: 'backup',
-    mixins: [BackupService],
-    settings: {
-        localServer: {
-            fusekiBackupsPath: CONFIG.BACKUP_FUSEKI_DATASETS_PATH,
-            otherDirsPaths: {
-                uploads: path.resolve(__dirname, '../uploads')
-            }
-        },
-        // Required only if you want to do automatic backups
-        cronJob: {
-            time: '0 0 4 * * *', // Every night at 4am
-            timeZone: 'Europe/Paris'
-        }
+  mixins: [BackupService],
+  settings: {
+    localServer: {
+      fusekiBackupsPath: path.resolve(__dirname, '../fuseki-backups'),
+      otherDirsPaths: {
+        uploads: path.resolve(__dirname, '../uploads')
+      }
+    },
+    cronJob: {
+      time: '0 4 * * *',
+      timeZone: 'Europe/Paris'
     }
+  },
+  actions: {
+    syncWithRemoteServer(ctx) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          let sftp = new Client();
+
+          await sftp.connect({
+            user: CONFIG.BACKUP_SERVER_USER,
+            password: CONFIG.BACKUP_SERVER_PASSWORD,
+            host: CONFIG.BACKUP_SERVER_HOST,
+            path: CONFIG.BACKUP_SERVER_PATH
+          });
+
+          fs.readdir(ctx.params.path, async function (err, files) {
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+                reject('Unable to scan directory: ' + err.message)
+            }
+            const now = Date.now();
+            files = files.map(function (fileName) {
+              return {
+                name: fileName,
+                diff: (now - fs.statSync(ctx.params.path + '/' + fileName).mtime) / 1000
+              };
+            }).filter(f=>f.diff<60)
+
+            for (var file of files) {
+              await sftp.put(ctx.params.path + '/' +file.name, process.env.SEMAPPS_FTP_PATH +file.name);
+            }
+
+            resolve()
+          });
+        } catch (e) {
+          reject (e)
+        }
+      });
+    }
+  }
 };
